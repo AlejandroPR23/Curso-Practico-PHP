924cd1c7d85065b86d1e9e8c8e41485194843786
AlejandroPR23 <perezrestrepoalejandro9@gmail.com>
2024-11-08 15:22:49 -0500

    👌 IMPROVE: Se agregan contenido en la paginá de op aritmeticos

---
+++ Operadores-Aritmeticos.php
@@ -0,0 +1,29 @@
+<?php
+
+$valorA=5;
+$valorB=2;
+
+$suma= $valorA + $valorB;
+
+echo "El resultado de la suma es: $suma \n";
+
+$resta= $valorA - $valorB;
+
+echo "El resultado de la resta es: $resta \n";
+
+$division= $valorA / $valorB;
+
+echo "El resultado de la división es: $division \n";
+
+$multiplicacion= $valorA * $valorB;
+
+echo "El resultado de la multiplicación es: $multiplicacion \n";
+
+$modulo= $valorA % $valorB;
+
+echo "El valor del modulo es: $modulo \n";
+
+$potenciacion= $valorA ** $valorB;
+
+echo "El resultado de la potenciación es: $potenciacion \n";
+
