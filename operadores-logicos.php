f6cc39f613e8b42370d8be69a22d89deb62a88e0
AlejandroPR23 <perezrestrepoalejandro9@gmail.com>
2024-11-08 13:45:52 -0500

    NEW: Create Operadores Logicos

--- operadores-logicos.php
+++ operadores-logicos.php
@@ -2,10 +2,42 @@
 
 $valorA = true;
 $valorB = true;
+$valorC= false;
 
-$resultado= $valorA && $valorB;
+// Ambos son verdaderos
+$resultado1= $valorA && $valorB;
 
-echo $resultado;
+var_dump(value: $resultado1); //TRUE
+
+// Uno es verdadero y otro es falso
+$resultado1= $valorA && $valorC;
+
+var_dump(value: $resultado1); // FALSE
+
+// Uno de los dos se cumple o ambos se cumplen
+$resultado1= $valorA || $valorC;
+
+var_dump(value: $resultado1); // True
+
+// SI ambos se cumple el restultado sera falso
+
+$resultado1= ($valorA xor $valorB); 
+
+var_dump(value: $resultado1); //FALSE
+
+// Negar algo
+
+$resultado1= !$valorA;
+
+var_dump(value: $resultado1); //FALSE
+
+
+
+
+
+
+
+
 
 
 
